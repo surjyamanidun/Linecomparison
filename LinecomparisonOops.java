9f9edabab5623d7767fc2712f62b58badb9a1af1
surjya <surjyamaniduan@gmail.com>
2021-09-25 13:06:49 +0530

    add:line eqal

--- LinecomparisonOops.java
+++ LinecomparisonOops.java
@@ -1,5 +1,35 @@
 package linecomparisonoops;
+import java.util.Scanner;
 
 public class LinecomparisonOops {
+	
 
-}
+	    public static void main(String[] args) {
+
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter x1:");
+	        int x1 = sc.nextInt();
+
+	        Scanner scd = new Scanner(System.in);
+	        System.out.print("Enter x2:");
+	        int x2 = scd.nextInt();
+
+	        Scanner scs = new Scanner(System.in);
+	        System.out.print("Enter y1:");
+	        int y1 = scs.nextInt();
+
+	        Scanner sca = new Scanner(System.in);
+	        System.out.print("Enter y2:");
+	        int y2 = sca.nextInt();
+
+	        LinecomparisonOops obj = new LinecomparisonOops();
+	        obj.calculate(x1, x2, y1, y2);
+	    }
+
+	    public void calculate(int x1, int x2, int y1, int y2) {
+	        int a = x2 - x1;
+	        int b = y2 - y1;
+	        System.out.println("Distance between the given coordinates are : " + Math.sqrt((a * a) + (b * b)));
+	    }
+	}
+
